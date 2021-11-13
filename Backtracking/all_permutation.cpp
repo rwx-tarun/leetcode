eb5ddfc0707953905d10686f5c677230ca6c9f77
rwx-tarun <78414267+rwx-tarun@users.noreply.github.com>
2021-11-13 09:20:22 +0530

    Update all_permutation.cpp

--- Backtracking/all_permutation.cpp
+++ Backtracking/all_permutation.cpp
@@ -1,20 +1,22 @@
-void solve(vector<int> &A,vector<vector<int> >&res,int s ,int e)
-{
-    if(s==e)
-    {
-        res.push_back(A);
-        return ;
+class Solution {
+    
+public:
+    void solve(vector<int>& a,vector<vector<int>>&res,int s,int e){
+        if(s==e)    
+        {
+            res.push_back(a);
+            return;
+        }
+        for(int i=s;i<e;i++)
+        {
+            swap(a[s],a[i]);
+            solve(a,res,s+1,e);
+            swap(a[s],a[i]);
+        }
     }
-    for(int i=s;i<A.size();i++)
-    {
-        swap(A[s],A[i]);
-        solve(A,res,s+1,e);
-        swap(A[i],A[s]);
+    vector<vector<int>> permute(vector<int>& a) {
+        vector<vector<int>>res;
+        solve(a,res,0,a.size());
+        return res;
     }
-}
-
-vector<vector<int> > Solution::permute(vector<int> &A) {
-    vector<vector<int>> res;
-    solve(A,res,0,A.size()-1);
-    return res;
-}
+};
