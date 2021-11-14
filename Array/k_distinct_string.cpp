51282c4a3c92a000e3c52cab6b2976c5406543e5
rwx-tarun <78414267+rwx-tarun@users.noreply.github.com>
2021-11-14 23:03:42 +0530

    Create k_distinct_string.cpp

---
+++ Array/k_distinct_string.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/kth-distinct-string-in-an-array/
+
+
+class Solution {
+public:
+    string kthDistinct(vector<string>& a, int k) {
+        unordered_map<string,int>mp;
+        for(int i=0;i<a.size();i++)
+            mp[a[i]]++;
+        for(int i =0;i<a.size();i++)
+        {
+            if(mp[a[i]]==1)
+            {
+                k--;
+                if(k==0)
+                    return a[i];
+            }
+            
+        }
+        return "";
+    }
+};
